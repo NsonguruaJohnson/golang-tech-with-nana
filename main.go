f6de5898b1cecf806aabb02571bfc320102fa8f8
NsonguruaJohnson <nsonguruajohnson@gmail.com>
2022-11-30 16:18:24 +0100

    Arrays and Slices

--- main.go
+++ main.go
@@ -7,6 +7,11 @@ func main() {
 	conferenceName := "Go Conference"
 	const conferenceTickets int = 50
 	var remainingTickets uint = 50
+	// var bookings = [50]string{} //an empty array
+	// var bookings [50]string // array
+	// var bookings = []string{} //an empty slice
+	// var bookings []string //slice
+	bookings := []string{} //slice
 
 	fmt.Printf("conferencetickets is %T, remainingTickets is %T, conferenceName is %T.\n", conferenceTickets, remainingTickets, conferenceName)
 	// fmt.Println("Welcome to", conferenceName, "booking application")
@@ -14,12 +19,30 @@ func main() {
 	fmt.Printf("We have a total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	var userName string
-	var userTickets int
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
 	// ask user for their name
-	// comment check
-	userName = "Tom"
-	userTickets = 2
-	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
+	fmt.Println("Enter your first name:")
+	fmt.Scan(&firstName)
 
+	fmt.Println("Enter your last name:")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email address:")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter number of tickets:")
+	fmt.Scan(&userTickets)
+
+	remainingTickets = remainingTickets - userTickets
+	// bookings[0] = firstName + "" + lastName
+	bookings = append(bookings, firstName+""+lastName)
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v.\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+
+	fmt.Println("Theres are all our bookings", bookings)
+	fmt.Printf("Theres are all our bookings: %v", bookings)
 }
